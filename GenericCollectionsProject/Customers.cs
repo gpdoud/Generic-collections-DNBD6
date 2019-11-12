f7a248c426cbb4647bc1069cf4c094bfcfd424c3
Greg Doud <gpdoud@gmail.com>
2019-11-12 16:11:04 -0500

    "Final commit"

---
+++ GenericCollectionsProject/Customers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsProject {
+
+    public class Customers {
+
+        List<Customer> customerList = new List<Customer>();
+
+        public void LoadCustomers() {
+            var cust1 = new Customer { Name = "P&G", State = "OH" };
+            var cust2 = new Customer { Name = "Microsoft", State = "WA" };
+            var cust3 = new Customer { Name = "Target", State = "MN" };
+
+            customerList.Add(cust1);
+            customerList.Add(cust2);
+            customerList.Add(cust3);
+        }
+
+        public void PrintCustomers() {
+            foreach(var cust in customerList) {
+                cust.
+                Console.WriteLine($"Customer {cust.Name} is in the state of {cust.State}");
+            }
+        }
+
+    }
+    public class Customer {
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
--- GenericCollectionsProject/Program.cs
+++ GenericCollectionsProject/Program.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCollectionsProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var customers = new Customers();
+            customers.LoadCustomers();
+            customers.PrintCustomers();
+
+        }
+        static void Run() { 
+
+            var rnd = new Random(); // used to generate random number
+
+            List<int> frames = new List<int>(10); // collection has only 10 items
+
+            for(var idx = 0; idx < 10; idx++) { // load 10 scores
+                var score = rnd.Next(0, 31);
+                frames.Add(score);
+            }
+
+            int total = 0;
+            foreach(var frame in frames) { // sum the scores
+                total += frame;
+            }
+
+            Console.WriteLine($"Game score is {total}");
+
         }
     }
 }
